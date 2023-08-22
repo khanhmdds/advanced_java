58299791c39e7697e1dc5516811d7c502f4dae4f
khanhmdds <khanhmd.ds@gmail.com>
2023-08-22 11:40:33 +0700

    add them bt

---
+++ Desktop/Java/2. Loop/prime_number_less_than_100/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = 2;
+
+        System.out.println("Prime numbers:");
+        while (N < 100) {
+            if (isPrime(N)) {
+                System.out.print(N + " ");
+            }
+            N++;
+        }
+
+        scanner.close();
+    }
+
+    // Function to check if a number is prime
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
